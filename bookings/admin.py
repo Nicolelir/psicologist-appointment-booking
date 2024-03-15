186ce67c54486d2fd87c19d520cdb43404cbf926
Nicole Rojas <nicole.eli.valdes@gmail.com>
2024-03-15 00:28:40 +0000

    Add styles to booklings

--- bookings/admin.py
+++ bookings/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from .models import Booking
 
 # Register your models here.
-admin.site.register(Booking)
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = [
+        'user', 'first_name', 'last_name', 'email', 'date', 'time']
+
 
 
--- bookings/forms.py
+++ bookings/forms.py
@@ -1,40 +1,45 @@
 from django import forms
-from djrichtextfield.widgets import RichTextWidget
+from django.contrib.auth.models import User
+from django.contrib import messages
 from .models import Booking
 
 class BookingForm(forms.ModelForm):
-    """A form to add a booking"""
+    """A form to create a booking"""
 
     def __init__(self, *args, **kwargs):
         user = kwargs.pop('user', None)
         super().__init__(*args, **kwargs)
         if user:
-            self.fields['email'].initial = user.email
+          self.fields['email'].initial = user.email
         self.fields['first_name'].required = True
         self.fields['last_name'].required = True
         self.fields['date'].required = True
 
         self.fields['first_name'].label = "First Name"
         self.fields['last_name'].label = "Last Name"
-        
         self.fields['date'].widget = forms.DateInput(attrs={'type': 'date'})
 
-    
+    def clean(self):
+        cleaned_data = super().clean()
+        date = cleaned_data.get('date')
+        time = cleaned_data.get('time')
+
+        # Check if a booking with the same date and time already exists
+        if (date and time and
+                Booking.objects.filter(date=date, time=time).exists()):
+            self.add_error('time', 'This date and time is already booked.')
+
+        instance = getattr(self, 'instance', None)
+        if instance:
+            # Exclude date and time from cleaning if they haven't been changed
+            if cleaned_data.get('date') == instance.date:
+                cleaned_data.pop('date', None)
+            if cleaned_data.get('time') == instance.time:
+                cleaned_data.pop('time', None)
+        return cleaned_data
 
     class Meta:
         model = Booking
         fields = [
-            'first_name', 'last_name', 'email', 'date', 'time', 'notes',
+            'first_name', 'last_name', 'email', 'date', 'time'
         ]
-     
-    notes = forms.CharField(widget=RichTextWidget())
-
-    labels = {
-            "user": "Booking By",
-            "first_name":"First Name",
-            "last_name":"Last Name",
-            "email":"Email",
-            "date":"Booking Date",
-            "time":"Booking Time",
-            "notes": "Notes",
-        }
