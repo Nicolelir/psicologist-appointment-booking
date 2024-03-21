8c0896bf87fb5374f8ecadfb76de33fb105aab1e
Nicole Rojas <nicole.eli.valdes@gmail.com>
2024-03-21 15:26:49 +0000

    Add alert message to home page and cakground colors to  django messages

--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from .forms import ContactForm 
 from .models import Contact
 
@@ -16,13 +17,23 @@ def contact_form_view(request):
     return render(request, 'home/index.html', {'form': form, 'message_sent': message_sent})
 
 def home_view(request):
+    message_sent = False  # Initialize the success message flag
+
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
             form.save()
-            # If the form is valid and saved, redirect to the same page to refresh the form
-            return redirect('home:home')
+            # Set the success message flag to True
+            message_sent = True
+            messages.success(request, "Your message has been sent successfully!")
     else:
-        form = ContactForm()  # Assign the form here for GET requests
+        form = ContactForm()
 
-    return render(request, 'home/index.html', {'form': form})
+    if not request.user.is_authenticated:
+        # User is not authenticated, display the message
+        message = "Invest in Yourself: Book a Session with Elvira, Your Trusted Mentor!"
+    else:
+        # User is authenticated, do not display the message
+        message = None
+
+    return render(request, 'home/index.html', {'form': form, 'message': message, 'message_sent': message_sent})
